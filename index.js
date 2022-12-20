428dec7da5356c5370572b9d19aad18c88fbab6a
Nishant Singh <ns777777711@gmail.com>
2022-12-20 15:02:50 +0530

    add

--- index.js
+++ index.js
@@ -6,13 +6,33 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/word", async (req, res) => {
+app.get("/",(req,res)=>{
+    res.json("hi")
+})
+
+app.get("/word",  (req, res) => {
     
     try {
         let words = ['Nish', 'Hello', 'acha' , "ok"];
         let word = words[Math.floor(Math.random()*words.length)];
         res.json({ status: "ok" , data:word});
-        
+
+    } catch (err) {
+      console.log(err)
+      res.json({ status: "error", error: err });
+    }
+  });
+  app.post("/user",  (req, res) => {
+    
+    try {
+        let name=req.body.name;
+        let difficulty= req.body.difficulty;
+        let score = req.body.score;
+
+        res.json({ status: "ok" , data:{
+            name,difficulty,score
+        }});
+
     } catch (err) {
       console.log(err)
       res.json({ status: "error", error: err });
